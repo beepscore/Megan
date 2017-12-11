ab435864e0b7102bb2d1c29b820e9feef5dbe11a
beepscore <support@beepscore.com>
2017-12-10 23:23:52 -0800

    Delete points

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ var about = require('./routes/about');
 var app = express();
 
 // app.locals scope is local to app. (i.e. global within app)
-app.locals.points = "8,713";
-// json acts like a literal for a javascript object
+// videodata.json file contains a javascript object literal
 app.locals.videodata = require('./data/videodata.json');
 
 // view engine setup
