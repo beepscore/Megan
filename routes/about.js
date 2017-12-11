5858037110b758b2ff8ae9db68a3155fbd918dff
beepscore <support@beepscore.com>
2017-12-10 22:52:17 -0800

    Add about.js and about.ejs. Add navigation.ejs and use it in index and about.

---
+++ routes/about.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET about page. */
+router.get('/', function(req, res, next) {
+
+  // render views/about.ejs to about.html
+  res.render('about', {
+    title: 'About',
+    name: 'Bucky'
+  });
+});
+
+module.exports = router;
